f595e25f38794f36d8a2f7f2c7edb20ae383bb7a
Jamy Golden <jamy@native.co.za>
2013-08-13 08:53:57 +0200

    Added comments for clarification

--- js/script.js
+++ js/script.js
@@ -4,25 +4,34 @@ $(document).ready(function() {
 	// before running this
 	$('#placeholder').nosInputPlaceholder();
 
+	// Input file
 	$('#inputfile').nosInputFile({
 		placeholder: 'Placeholder Text'
 	});
 
+	// Form Select
 	$('#select').nosFormSelect();
 
+	// Form select with defaultDropdown
 	$('#select-alt').nosFormSelect({
 		defaultDropdown: true
 	});
 
+	// Input Checkbox
 	$('input[type="checkbox"]').nosInputCheckbox();
 
+	// Input Radio
 	$('input[type="radio"]').nosInputRadio();
 
+	// Tooltip
+	$('.tooltip').nosTooltip();
+
+	// Toggle form elements for informational purposes
+	// With this you can see how selecting a custom 
+	// form element selects it's hidden counterpart.
 	$('#toggle-form-elements').click(function(e){
 		e.preventDefault();
 		
 		$('div').next('input, select').toggle();
 	});
-
-	$('.tooltip').nosTooltip();
 }); // document.ready()
